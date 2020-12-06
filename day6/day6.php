1d2a200f6f83676d44372ff48eb8cce8ee8d31cc
Kevin Boutsen <boutsen@gmail.com>
2020-12-06 10:00:08 +0100

    Day6

---
+++ day6/day6.php
@@ -0,0 +1,54 @@
+<?php 
+function readLines($filename)
+{	
+	$array = null;
+	$fp = @fopen($filename, 'r'); 
+
+	// Add each line to an array
+	if ($fp) {
+	   $array = explode("\n", fread($fp, filesize($filename)));
+	}
+	return $array;
+}
+
+function sanitize($array) {
+	$result = [];
+	
+	$pp = "";
+	foreach($array as $line)
+	{
+		if ( empty(trim($line)) ){
+			$d = explode(" ",trim($pp));
+			array_push($result,$d);
+			$pp = "";
+		}
+		$pp .= "$line ";
+	}
+	return $result;
+}
+
+$in = readLines("input.txt");
+
+$groups = sanitize($in);
+$sum = 0;
+$sum2 = 0;
+foreach( $groups as $group )
+{
+	$sum += count( array_unique( str_split(preg_replace("/[^a-zA-Z]/", "", implode("",$group)))));
+	
+	
+	$intersect = str_split(preg_replace("/[^a-zA-Z]/", "",$group[0]));
+	for ( $i = 1; $i < count($group); $i++)
+	{
+		$intersect = array_intersect($intersect,str_split(preg_replace("/[^a-zA-Z]/", "",$group[$i])));
+	}
+	
+	$sum2 += count($intersect); 
+}
+
+
+echo "Solution day6-part1: " . $sum. PHP_EOL;
+echo "Solution day6-part2: " . $sum2 . PHP_EOL;
+
+
+?>
