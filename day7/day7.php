f94975a2ad02429f4df06b1611f5bd3c8e8ea3fb
Kevin Boutsen <kevin.boutsen@ctg.com>
2020-12-07 10:37:14 +0100

    day7

---
+++ day7/day7.php
@@ -0,0 +1,63 @@
+<?php
+function readLines($filename)
+{	
+	$array = null;
+	$fp = @fopen($filename, 'r'); 
+
+	// Add each line to an array
+	if ($fp) {
+	   $array = explode("\n", fread($fp, filesize($filename)));
+	}
+	return $array;
+}
+
+function parse_rules($rules)
+{
+	$result = [];
+	
+	foreach( $rules as $line)
+	{
+		$matches = null;
+		preg_match_all("/(\w+ \w+ bags)|(\d+ \w+ \w+)/",$line,$matches);
+		$content = [];
+		for( $i = 1; $i < count($matches[0]); $i++ )
+		{
+			$split = preg_split("/(?<=[0-9]) (?=[a-z]+)/i",$matches[0][$i]);
+			if ( !empty($split[1]) ) 
+				$content[$split[1]] = intval($split[0]);
+		}
+		$result[str_replace(" bags","",$matches[0][0])] = $content;
+	}
+	
+	return $result;
+}
+
+function get_count($rules, $color)
+{
+	$ret = 0;
+
+	foreach ($rules[$color] as $k => $v)
+		$ret += $v * get_count($rules, $k);
+
+	return $ret + 1;
+}
+
+function get_colors($rules, $color)
+{
+	$ret = [];
+
+	foreach ($rules as $p => $c)
+		if (array_key_exists($color, $c))
+			$ret = array_merge($ret, [$p], get_colors($rules, $p));
+
+	return array_unique($ret);
+}
+
+
+
+$in = readLines("input.txt");
+$bags = parse_rules($in);
+
+echo "Solution day7-part1: " . count(get_colors($bags, 'shiny gold')) . PHP_EOL;
+echo "Solution day7-part2: " . (get_count($bags, 'shiny gold')-1) . PHP_EOL;
+
