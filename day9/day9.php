bc379cd8883e3624f39334b2f675a11ae112e132
Kevin Boutsen <boutsen@gmail.com>
2020-12-09 09:20:59 +0100

    day 9

---
+++ day9/day9.php
@@ -0,0 +1,52 @@
+<?php 
+function readLines($filename)
+{	
+	$array = null;
+	$fp = @fopen($filename, 'r'); 
+
+	// Add each line to an array
+	if ($fp) {
+	   $array = explode("\n", fread($fp, filesize($filename)));
+	}
+	return array_map("intval",$array);
+}
+
+function findInvalidSum($arr, $preamble)
+{
+	$i = $preamble-1;	
+	while( $i++ < count($arr) )
+	{
+		$valid = false;
+		for($j=$i-$preamble;$j<$i;$j++)
+			for($k=$j;$k<$i;$k++)
+				if($arr[$j] + $arr[$k] == $arr[$i])
+					$valid = true;
+		if( !$valid )
+			return $arr[$i];
+	}
+}
+
+function findWeakness($arr,$invalid)
+{
+	for($i=0;$i<count($arr);$i++)
+	{
+		$sum = $arr[$i];
+		$array = [$sum];
+		for($j=$i+1;$j<count($arr);$j++)
+		{
+			$sum+=$arr[$j];
+			$array[] = $arr[$j];
+			if($sum == $invalid)
+				return((min($array) + max($array)));
+		}
+	}
+}
+
+
+$numbers = readLines("input.txt");
+$preamble = 25;
+
+$calc = findInvalidSum($numbers,$preamble);
+
+echo "Solution day9-part1: " . $calc . PHP_EOL;
+echo "Solution day9-part2: " . findWeakness($numbers,$calc) . PHP_EOL;
