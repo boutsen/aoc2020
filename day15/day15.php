159eb4babc12aa1b49663897bfde69d17f136edd
Kevin Boutsen <kevin.boutsen@ctg.com>
2020-12-15 08:51:15 +0100

    day15

---
+++ day15/day15.php
@@ -0,0 +1,43 @@
+<?php
+
+$input = "0,12,6,13,20,1,17";
+
+$in = explode(",",$input);
+
+
+function solve($arr,$max_i)
+{
+	$spoken = [];
+	foreach( $arr as $i => $v)
+		$spoken[$v] = $i+1;
+	
+	$last_spoken = end($arr);
+	
+	for( $i = count($arr); $i < $max_i; $i++)
+	{
+		$next = !array_key_exists($last_spoken,$spoken) ? 0 : $i-$spoken[$last_spoken];
+		$spoken[$last_spoken] = $i;
+		$last_spoken = $next;
+	}
+	return $last_spoken;
+}
+
+
+
+/// Part 1
+$start_time = microtime(true); 
+$part1 = solve($in,2020);
+$end_time = microtime(true); 
+$execution_time = ($end_time - $start_time); 
+
+echo "Solution day15-part1: " . $part1 . " and took " . round($execution_time*1000,2) . " ms." . PHP_EOL;
+
+ini_set("memory_limit", "-1");
+// Part 2
+$start_time = microtime(true); 
+$part2 = solve($in,30000000);
+$end_time = microtime(true); 
+$execution_time = ($end_time - $start_time); 
+
+
+echo "Solution day15-part2: " . $part2 . " and took " . round($execution_time,2) . " sec." . PHP_EOL;
